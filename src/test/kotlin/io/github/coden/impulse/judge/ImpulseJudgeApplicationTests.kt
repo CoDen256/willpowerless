7ff7d83f69795496082ef18c7a1e934a87430bbc
Denys Chernyshov <den.blackshov@gmail.com>
2024-11-04 21:48:47 +0100

    init

---
+++ src/test/kotlin/io/github/coden/impulse/judge/ImpulseJudgeApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.coden.impulse.judge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImpulseJudgeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
