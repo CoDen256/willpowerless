c0e746378b0f840fb8961c0d94bc36e8efa0ccd3
Denys Chernyshov <den.blackshov@gmail.com>
2025-02-11 21:23:32 +0100

    unset exec bit from busybox

--- openwrt.setup.sh
+++ openwrt.setup.sh
@@ -179,6 +179,9 @@ mkdir /home/diagnostics
 # or verify that the /etc/passwd contains /bin/ash as the last column
 useradd -r -s /bin/ash -d /home/diagnostics diagnostics
 echo -e "abc\nabc" | passwd "diagnostics"
+chmod u-s /bin/busybox # DISABLE EXECUTION BIT. IF diagnostics runs /bin/busybox, then everything is done via effective euid(0)==root
+# shell and all tools like ls cat etc, are actually run effectively by root with this bit
+# hopefully it won't crash any other service
 
 # or verify thath the /etc/group contains
 cat /etc/passwd
