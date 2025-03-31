91f3cf58af87737f2cc75888a808297a0a8261e6
Denys Chernyshov <den.blackshov@gmail.com>
2025-03-31 21:03:03 +0200

    fix

--- src/test/kotlin/io/github/coden256/judge/WellpassRuleTest.kt
+++ src/test/kotlin/io/github/coden256/judge/WellpassRuleTest.kt
@@ -1,11 +1,9 @@
 package io.github.coden256.judge
 
-import io.github.coden256.judge.api.Match
-import io.github.coden256.judge.rules.WellpassRule
 import io.github.coden.wellpass.api.CheckIn
 import io.github.coden.wellpass.api.CheckIns
-import org.junit.jupiter.api.Assertions.*
-
+import io.github.coden256.judge.rules.WellpassRule
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 
@@ -15,23 +13,23 @@ class WellpassRuleTest {
     fun test() {
         // setup
         assertEquals(
-            Match(true, "ok"), WellpassRule().test(CheckIns(listOf(
-            CheckIn("KOSMOS Boulderhalle Leipzig", LocalDateTime.now())))))
+            true, WellpassRule().test(CheckIns(listOf(
+            CheckIn("KOSMOS Boulderhalle Leipzig", LocalDateTime.now())))).allowed)
         assertEquals(
-            Match(true, "ok"), WellpassRule().test(CheckIns(listOf(
-            CheckIn("Ute Stephan Yoga Studio Plagwitz", LocalDateTime.now())))))
+            true, WellpassRule().test(CheckIns(listOf(
+            CheckIn("Ute Stephan Yoga Studio Plagwitz", LocalDateTime.now())))).allowed)
         assertEquals(
-            Match(true, "ok"), WellpassRule().test(CheckIns(listOf(
-            CheckIn("Onyx Boulderspot Leipzig", LocalDateTime.now())))))
+            true, WellpassRule().test(CheckIns(listOf(
+            CheckIn("Onyx Boulderspot Leipzig", LocalDateTime.now())))).allowed)
         assertEquals(
-            Match(true, "ok"), WellpassRule().test(CheckIns(listOf(
-            CheckIn("Fitness First Leipzig - Messehof", LocalDateTime.now())))))
+            true, WellpassRule().test(CheckIns(listOf(
+            CheckIn("Fitness First Leipzig - Messehof", LocalDateTime.now())))).allowed)
         assertEquals(
-            Match(true, "ok"), WellpassRule().test(CheckIns(listOf(
-            CheckIn("Kletterhalle NO LIMIT Leipzig", LocalDateTime.now())))))
+            true, WellpassRule().test(CheckIns(listOf(
+            CheckIn("Kletterhalle NO LIMIT Leipzig", LocalDateTime.now())))).allowed)
         assertEquals(
-            Match(true, "ok"), WellpassRule().test(CheckIns(listOf(
-            CheckIn("FIT/ONE Leipzig", LocalDateTime.now())))))
+            true, WellpassRule().test(CheckIns(listOf(
+            CheckIn("FIT/ONE Leipzig", LocalDateTime.now())))).allowed)
 
         assertEquals(false, WellpassRule().test(CheckIns(listOf(
             CheckIn("Vinya Loft Yogastudio Bremen", LocalDateTime.now())))).allowed)
