d9453d35649f1c1ca6102c9adb23fd23553855c6
Denys Chernyshov <den.blackshov@gmail.com>
2025-04-08 17:29:56 +0200

    update

--- router/scripts/guard-wrapper.c
+++ router/scripts/guard-wrapper.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// LF 32-bit LSB executable, MIPS, MIPS32 rel2 version 1 (SYSV), dynamically linked, interpreter /lib/ld-musl-mipsel-sf.so.1, no section header
+
+// mips-unknown-linux-muslsf.tar.xz
+// mipsel-unknown-linux-muslsf.tar.xz
+
+
 int main(int argc, char** argv) {
     printf("%d", geteuid());
     return 0;
