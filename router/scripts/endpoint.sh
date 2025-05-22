9dc6e6122e051921deb3281a378471cb4b625678
Denys Chernyshov <den.blackshov@gmail.com>
2025-05-22 22:12:42 +0200

    Update endpoint

--- router/scripts/endpoint.sh
+++ router/scripts/endpoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 mkdir "/etc/judge"
-echo "http://willpowerless-judge.up.railway.app/rulings/dev/openwrt" > /etc/judge/endpoint
+echo "http://217.154.222.188/rulings/dev/openwrt" > /etc/judge/endpoint
 
 chmod 664 /etc/judge/endpoint
